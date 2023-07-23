00bd3bdbcf5fe02b7c3ef4ee00a6b7bb01242b05
Srivastava <Pankaj.Srivastava@baltimorecity.gov>
2023-07-23 12:03:30 -0700

    first commit

---
+++ src/main/java/net/javaguides/mssql/repository/AlleyRepository.java
@@ -0,0 +1,8 @@
+package net.javaguides.mssql.repository;
+
+import net.javaguides.mssql.model.Alley;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlleyRepository extends JpaRepository<Alley, Long>{
+
+}
